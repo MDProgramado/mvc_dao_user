8602d1b0180d5fcf157116e5c207d0b9bc28c499
Maicon Douglas <mdprogamacao@outlook.com>
2026-09-25 10:26:55 -0300

    Criando estrutura do model, o dao, realizando conecção com o banco de dados criando função para caso não tenha sido a tabela, criando controler incial de inserir e criando interface simples com javax swing

---
+++ src/main/java/com/pizzaria/test/TesteConexao.java
@@ -0,0 +1,12 @@
+package com.pizzaria.test;
+
+import com.pizzaria.dao.ConexaoDAO;
+
+import java.sql.SQLException;
+
+public class TesteConexao {
+    public static void main(String[] args) throws SQLException {
+
+
+    }
+}
